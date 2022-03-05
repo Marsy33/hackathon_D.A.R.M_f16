782dd184960307cb85ca068be3a491c16b94918b
alsu <9.alsulu@gmail.com>
2022-03-05 17:28:54 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>
 }
 
 export default App;
