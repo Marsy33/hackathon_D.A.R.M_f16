d9a3eef6eb1dc511bb6ce58569274a0842ddd465
Marsy33 <khudyk0302@gmail.com>
2022-03-05 16:15:23 +0200

    conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import HelpForm from "./components/Form/helpForm.jsx";
+import { Main } from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <HelpForm />
     </div>
   );
